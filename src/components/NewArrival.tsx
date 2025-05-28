3cf8bf8ddfd4959b3f31289639eecd119f4f0a2e
Anthony Frank A. Ordoñez <143399325+AnthonyFrank-Ordonez@users.noreply.github.com>
2025-05-28 21:29:08 +0800

    Improve Dynamic layout

--- src/components/NewArrival.tsx
+++ src/components/NewArrival.tsx
@@ -20,34 +20,40 @@ export default function NewArrival({
 }: NewArrivalProps) {
 	return (
 		<section className='bg-gray-100'>
-			<ScrollFadeSection className='flex flex-col px-12 py-12 md:flex-row'>
+			<ScrollFadeSection className='flex flex-col gap-10 px-12 py-12 md:flex-row md:gap-5 lg:flex-row lg:gap-5 xl:flex-row xl:gap-5'>
 				<div className='flex flex-1 flex-col'>
-					<h2 className='max-w-sm text-9xl font-extrabold'>New Arrival</h2>
-					<p className='mt-7 text-4xl font-bold'>{name}</p>
-					<p className='text-3xl font-normal'>${price}</p>
-					<p className='mt-5 max-w-sm font-normal text-gray-500'>
+					<h2 className='text-7xl font-extrabold md:max-w-sm md:text-7xl lg:max-w-sm lg:text-8xl xl:max-w-sm xl:text-9xl'>
+						New Arrival
+					</h2>
+					<p className='mt-3 text-2xl font-bold md:mt-3 md:text-3xl lg:mt-4 lg:text-3xl xl:mt-5 xl:text-4xl'>
+						{name}
+					</p>
+					<p className='text-xl font-normal md:text-xl lg:text-2xl xl:text-3xl'>
+						${price}
+					</p>
+					<p className='mt-5 text-sm font-normal text-gray-500 md:mt-3 md:max-w-sm md:text-sm lg:mt-4 lg:max-w-md lg:text-lg xl:mt-5 xl:max-w-lg'>
 						{description}
 					</p>
 					<div></div>
 					<Link
 						to='/'
-						className='mt-7 w-58 items-center rounded-2xl border bg-black px-3 py-4 text-center font-bold text-white transition-colors duration-300 hover:bg-gray-800/70'
+						className='mt-7 items-center rounded-2xl border bg-black py-4 text-center font-bold text-white transition-colors duration-300 hover:bg-gray-800/70 md:mt-5 md:w-52 md:px-2 md:py-3 lg:w-55 lg:px-3 lg:py-4 xl:w-68 xl:px-3 xl:py-4'
 					>
 						Add to Cart
 					</Link>
-					<div className='w-58'>
-						<p className='text-md text-center font-light'>
+					<div className='mt-2 w-full md:w-52 lg:w-54 xl:w-66'>
+						<p className='text-center text-sm font-light xl:text-lg'>
 							Only {stock} stocks are available!
 						</p>
 					</div>
 				</div>
 
-				<div className='flex flex-1 items-center justify-end'>
-					<div className='relative flex h-[38rem] w-[35rem] items-center justify-center overflow-hidden rounded-md bg-gray-200'>
+				<div className='flex flex-1 items-center justify-center md:justify-end lg:justify-end xl:justify-end'>
+					<div className='relative flex h-[28rem] w-[30rem] items-center justify-center overflow-hidden rounded-md bg-gray-200/50 md:h-[27rem] md:w-[22rem] lg:h-[33em] lg:w-[26rem] xl:h-[38rem] xl:w-[32rem]'>
 						<img
 							src={imageUrl}
 							alt={imageAlt}
-							className='mx-auto h-[30rem] w-84 object-cover'
+							className='mx-auto h-[23rem] w-54 object-cover md:h-[22rem] md:w-62 lg:h-[26rem] lg:w-70 xl:h-[30rem] xl:w-84'
 						/>
 					</div>
 				</div>
--- src/routes/products/books/index.tsx
+++ src/routes/products/books/index.tsx
@@ -1,6 +1,7 @@
 import NewArrival from '@/components/NewArrival';
 import { ScrollFadeSection } from '@/components/ScrollFadeSection';
 import { createFileRoute } from '@tanstack/react-router';
+import { FaRegHeart, FaRegStar, FaStar } from 'react-icons/fa';
 import { CiSearch } from 'react-icons/ci';
 
 export const Route = createFileRoute('/products/books/')({
@@ -9,241 +10,133 @@ export const Route = createFileRoute('/products/books/')({
 
 function BooksIndex() {
 	return (
-		<div className='col-span-1 md:col-span-12'>
+		<div className='col-span-1 md:col-span-12 lg:col-span-12 xl:col-span-12'>
 			{/* New Arrivals */}
 			<NewArrival />
 
 			{/* Sortby and Products List*/}
 			<section>
-				<ScrollFadeSection className='mt-12 mb-3 flex items-center justify-between px-12'>
-					<div className='relative flex flex-row items-center gap-3'>
-						<input
-							type='input'
-							placeholder='Enter Book Title'
-							className='rounded-xl border border-gray-400 px-4 py-1 font-light text-gray-700 focus:border-gray-500 focus:outline-none'
-						/>
+				<div className='mt-12 grid grid-cols-1 py-12 md:grid-cols-12 lg:grid-cols-12'>
+					<ScrollFadeSection className='col-span-1 border-gray-200 md:col-span-10 lg:col-span-10'>
+						<div className='grid grid-cols-2 gap-11 border-r-2 border-gray-200 px-12 md:grid-cols-4 lg:grid-cols-4'>
+							<div className='h-full w-62 rounded-lg bg-white shadow-md'>
+								<img
+									src='https://picsum.photos/id/1015/400/600'
+									alt='book cover'
+									className='h-64 w-full object-cover'
+								/>
 
-						<CiSearch className='absolute right-3' />
-					</div>
+								<div className='p-5'>
+									<h3 className='text-xl font-bold'>Book Title 1</h3>
 
-					<div className='flex flex-row items-center gap-3'>
-						<p className='text-md font-light'>Sort By:</p>
-						<select className='border-b border-gray-400 px-2 font-light text-gray-700 focus:border-gray-500 focus:outline-none'>
-							<option value='newest'>Newest</option>
-							<option value='oldest'>Oldest</option>
-							<option value='price-asc'>Price: Low to High</option>
-							<option value='price-desc'>Price: High to Low</option>
-							<option value='popularity'>Popularity</option>
-							<option value='rating'>Rating</option>
-						</select>
-					</div>
-				</ScrollFadeSection>
+									<div className='flex items-center gap-1'>
+										<div className='flex items-center gap-1 text-yellow-400'>
+											<FaStar />
+											<FaStar />
+											<FaStar />
+											<FaStar />
+											<FaRegStar />
+										</div>
+										(1,000+)
+									</div>
 
-				<ScrollFadeSection className='grid grid-cols-2 gap-3 px-12 md:grid-cols-4'>
-					<div className='rounded-lg bg-white shadow-md'>
-						<img
-							src='https://picsum.photos/id/1015/400/600'
-							alt='book cover'
-							className='h-88 w-full object-cover'
-						/>
+									<div className='mt-2 flex items-center justify-between py-1'>
+										<p className='text-gray-500'>Price</p>
+										<p className='font-normal'>$19.99</p>
+									</div>
 
-						<div className='p-5'>
-							<h3 className='text-xl font-bold'>Book Title 1</h3>
-							<p className='text-gray-600'>Author Name</p>
-							<p className='mt-2 text-lg font-semibold'>$19.99</p>
-							<button className='mt-2 w-full cursor-pointer rounded-full border bg-black px-4 py-2 text-white transition-colors duration-300 hover:bg-black/80'>
-								Add to Cart
-							</button>
+									<div className='flex items-center justify-between py-1'>
+										<p className='text-gray-500'>Stocks</p>
+										<p className='font-normal'>35+</p>
+									</div>
+
+									<div className='flex items-center justify-between py-1'>
+										<p className='text-gray-500'>Author</p>
+										<p className='font-normal'>Author Name</p>
+									</div>
+
+									<div className='mt-3 flex flex-row items-center gap-2'>
+										<button className='w-full flex-1 cursor-pointer rounded-md border bg-black px-4 py-2 text-white transition-colors duration-300 hover:bg-black/80'>
+											Add to Cart
+										</button>
+
+										<div className='flex h-10 w-11 cursor-pointer items-center justify-center rounded-md border text-center transition-colors duration-300 hover:bg-gray-400/10'>
+											<FaRegHeart size={24} />
+										</div>
+									</div>
+								</div>
+							</div>
 						</div>
-					</div>
+					</ScrollFadeSection>
 
-					<div className='rounded-lg bg-white shadow-md'>
-						<img
-							src='https://picsum.photos/id/1015/400/600'
-							alt='book cover'
-							className='h-88 w-full object-cover'
-						/>
+					{/* Filter Section */}
+					<ScrollFadeSection>
+						<aside className='hidden h-screen w-48 px-5 lg:col-span-2 lg:block'>
+							{/* Search */}
+							<h2 className='text-md w-34 text-left font-bold text-black'>
+								Search By Title:
+							</h2>
+							<div className='relative mt-1 flex flex-col gap-2 overflow-hidden'>
+								<input
+									type='text'
+									placeholder='Enter Book Title'
+									className='rounded-sm border border-gray-400 px-2 py-1 font-light text-gray-700 focus:border-gray-500 focus:outline-none'
+								/>
+								<CiSearch className='absolute top-2 right-2 text-gray-400' />
+							</div>
 
-						<div className='p-5'>
-							<h3 className='text-xl font-bold'>Book Title 1</h3>
-							<p className='text-gray-600'>Author Name</p>
-							<p className='mt-2 text-lg font-semibold'>$19.99</p>
-							<button className='mt-2 w-full cursor-pointer rounded-full border bg-black px-4 py-2 text-white transition-colors duration-300 hover:bg-black/80'>
-								Add to Cart
-							</button>
-						</div>
-					</div>
+							<h2 className='text-md mt-7 w-34 text-left font-bold text-black'>
+								Filter Books By:
+							</h2>
 
-					<div className='rounded-lg bg-white shadow-md'>
-						<img
-							src='https://picsum.photos/id/1015/400/600'
-							alt='book cover'
-							className='h-88 w-full object-cover'
-						/>
+							{/* Dropdown area */}
+							<div className='mt-5 flex flex-col gap-5'>
+								<div className='flex flex-col gap-1'>
+									<p className='text-md text-gray-500'>Date:</p>
+									<select className='text-md w-full border-b border-gray-400 pb-1 font-light text-gray-700 focus:outline-none'>
+										<option value='latest'>Latest</option>
+										<option value='last-week'>Last Week</option>
+										<option value='month'>A Month Ago</option>
+										<option value='year'>A Year Ago</option>
+									</select>
+								</div>
 
-						<div className='p-5'>
-							<h3 className='text-xl font-bold'>Book Title 1</h3>
-							<p className='text-gray-600'>Author Name</p>
-							<p className='mt-2 text-lg font-semibold'>$19.99</p>
-							<button className='mt-2 w-full cursor-pointer rounded-full border bg-black px-4 py-2 text-white transition-colors duration-300 hover:bg-black/80'>
-								Add to Cart
-							</button>
-						</div>
-					</div>
+								<div className='flex flex-col gap-2'>
+									<p className='text-md text-gray-500'>Price:</p>
+									<select className='text-md w-full border-b border-gray-400 pb-1 font-light text-gray-700 focus:outline-none'>
+										<option value='price-asc'>High to Low</option>
+										<option value='price-desc'>Low to High</option>
+										<option value='free'>Free</option>
+									</select>
+								</div>
 
-					<div className='rounded-lg bg-white shadow-md'>
-						<img
-							src='https://picsum.photos/id/1015/400/600'
-							alt='book cover'
-							className='h-88 w-full object-cover'
-						/>
+								<div className='flex flex-col gap-2'>
+									<p className='text-md text-gray-500'>Ratings:</p>
+									<select className='text-md w-full border-b border-gray-400 pb-1 font-light text-gray-700 focus:outline-none'>
+										<option value='latest'>Highest to Lowest</option>
+										<option value='last-week'>Lowest to Highest</option>
+									</select>
+								</div>
 
-						<div className='p-5'>
-							<h3 className='text-xl font-bold'>Book Title 1</h3>
-							<p className='text-gray-600'>Author Name</p>
-							<p className='mt-2 text-lg font-semibold'>$19.99</p>
-							<button className='mt-2 w-full cursor-pointer rounded-full border bg-black px-4 py-2 text-white transition-colors duration-300 hover:bg-black/80'>
-								Add to Cart
-							</button>
-						</div>
-					</div>
+								<div className='flex flex-col gap-2'>
+									<p className='text-md text-gray-500'>Author:</p>
+									<select className='text-md w-full border-b border-gray-400 pb-1 font-light text-gray-700 focus:outline-none'>
+										<option value='author-name'>Author Name</option>
+										<option value='author-name'>Author Name</option>
+									</select>
+								</div>
 
-					<div className='rounded-lg bg-white shadow-md'>
-						<img
-							src='https://picsum.photos/id/1015/400/600'
-							alt='book cover'
-							className='h-88 w-full object-cover'
-						/>
-
-						<div className='p-5'>
-							<h3 className='text-xl font-bold'>Book Title 1</h3>
-							<p className='text-gray-600'>Author Name</p>
-							<p className='mt-2 text-lg font-semibold'>$19.99</p>
-							<button className='mt-2 w-full cursor-pointer rounded-full border bg-black px-4 py-2 text-white transition-colors duration-300 hover:bg-black/80'>
-								Add to Cart
-							</button>
-						</div>
-					</div>
-
-					<div className='rounded-lg bg-white shadow-md'>
-						<img
-							src='https://picsum.photos/id/1015/400/600'
-							alt='book cover'
-							className='h-88 w-full object-cover'
-						/>
-
-						<div className='p-5'>
-							<h3 className='text-xl font-bold'>Book Title 1</h3>
-							<p className='text-gray-600'>Author Name</p>
-							<p className='mt-2 text-lg font-semibold'>$19.99</p>
-							<button className='mt-2 w-full cursor-pointer rounded-full border bg-black px-4 py-2 text-white transition-colors duration-300 hover:bg-black/80'>
-								Add to Cart
-							</button>
-						</div>
-					</div>
-
-					<div className='rounded-lg bg-white shadow-md'>
-						<img
-							src='https://picsum.photos/id/1015/400/600'
-							alt='book cover'
-							className='h-88 w-full object-cover'
-						/>
-
-						<div className='p-5'>
-							<h3 className='text-xl font-bold'>Book Title 1</h3>
-							<p className='text-gray-600'>Author Name</p>
-							<p className='mt-2 text-lg font-semibold'>$19.99</p>
-							<button className='mt-2 w-full cursor-pointer rounded-full border bg-black px-4 py-2 text-white transition-colors duration-300 hover:bg-black/80'>
-								Add to Cart
-							</button>
-						</div>
-					</div>
-
-					<div className='rounded-lg bg-white shadow-md'>
-						<img
-							src='https://picsum.photos/id/1015/400/600'
-							alt='book cover'
-							className='h-88 w-full object-cover'
-						/>
-
-						<div className='p-5'>
-							<h3 className='text-xl font-bold'>Book Title 1</h3>
-							<p className='text-gray-600'>Author Name</p>
-							<p className='mt-2 text-lg font-semibold'>$19.99</p>
-							<button className='mt-2 w-full cursor-pointer rounded-full border bg-black px-4 py-2 text-white transition-colors duration-300 hover:bg-black/80'>
-								Add to Cart
-							</button>
-						</div>
-					</div>
-
-					<div className='rounded-lg bg-white shadow-md'>
-						<img
-							src='https://picsum.photos/id/1015/400/600'
-							alt='book cover'
-							className='h-88 w-full object-cover'
-						/>
-
-						<div className='p-5'>
-							<h3 className='text-xl font-bold'>Book Title 1</h3>
-							<p className='text-gray-600'>Author Name</p>
-							<p className='mt-2 text-lg font-semibold'>$19.99</p>
-							<button className='mt-2 w-full cursor-pointer rounded-full border bg-black px-4 py-2 text-white transition-colors duration-300 hover:bg-black/80'>
-								Add to Cart
-							</button>
-						</div>
-					</div>
-
-					<div className='rounded-lg bg-white shadow-md'>
-						<img
-							src='https://picsum.photos/id/1015/400/600'
-							alt='book cover'
-							className='h-88 w-full object-cover'
-						/>
-
-						<div className='p-5'>
-							<h3 className='text-xl font-bold'>Book Title 1</h3>
-							<p className='text-gray-600'>Author Name</p>
-							<p className='mt-2 text-lg font-semibold'>$19.99</p>
-							<button className='mt-2 w-full cursor-pointer rounded-full border bg-black px-4 py-2 text-white transition-colors duration-300 hover:bg-black/80'>
-								Add to Cart
-							</button>
-						</div>
-					</div>
-
-					<div className='rounded-lg bg-white shadow-md'>
-						<img
-							src='https://picsum.photos/id/1015/400/600'
-							alt='book cover'
-							className='h-88 w-full object-cover'
-						/>
-
-						<div className='p-5'>
-							<h3 className='text-xl font-bold'>Book Title 1</h3>
-							<p className='text-gray-600'>Author Name</p>
-							<p className='mt-2 text-lg font-semibold'>$19.99</p>
-							<button className='mt-2 w-full cursor-pointer rounded-full border bg-black px-4 py-2 text-white transition-colors duration-300 hover:bg-black/80'>
-								Add to Cart
-							</button>
-						</div>
-					</div>
-
-					<div className='rounded-lg bg-white shadow-md'>
-						<img
-							src='https://picsum.photos/id/1015/400/600'
-							alt='book cover'
-							className='h-88 w-full object-cover'
-						/>
-
-						<div className='p-5'>
-							<h3 className='text-xl font-bold'>Book Title 1</h3>
-							<p className='text-gray-600'>Author Name</p>
-							<p className='mt-2 text-lg font-semibold'>$19.99</p>
-							<button className='mt-2 w-full cursor-pointer rounded-full border bg-black px-4 py-2 text-white transition-colors duration-300 hover:bg-black/80'>
-								Add to Cart
-							</button>
-						</div>
-					</div>
-				</ScrollFadeSection>
+								<div className='flex flex-col gap-2'>
+									<p className='text-md text-gray-500'>Genre:</p>
+									<select className='text-md w-full border-b border-gray-400 pb-1 font-light text-gray-700 focus:outline-none'>
+										<option value='genre'>Genre</option>
+										<option value='genre'>Genre</option>
+									</select>
+								</div>
+							</div>
+						</aside>
+					</ScrollFadeSection>
+				</div>
 			</section>
 		</div>
 	);
