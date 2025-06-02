7beb533c1f50db7ac048a5ddc6d1f8e4ddd0afd5
Anthony Frank A. Ordoñez <143399325+AnthonyFrank-Ordonez@users.noreply.github.com>
2025-06-02 21:18:59 +0800

    Added Require Prisma FIles

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,10 +1,17 @@
 import { betterAuth } from 'better-auth';
+import { prismaAdapter } from 'better-auth/adapters/prisma';
+import { reactStartCookies } from 'better-auth/react-start';
+import prisma from '@/utils/prisma';
 
 export const auth = betterAuth({
+	database: prismaAdapter(prisma, {
+		provider: 'postgresql',
+	}),
 	socialProviders: {
 		github: {
 			clientId: process.env.GITHUB_CLIENT_ID! as string,
 			clientSecret: process.env.GITHUB_CLIENT_SECRET! as string,
 		},
 	},
+	plugins: [reactStartCookies()],
 });
