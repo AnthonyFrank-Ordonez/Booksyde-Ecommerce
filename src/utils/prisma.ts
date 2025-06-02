7f9440353bf1c58636210d0bbebd7aeed10400da
Anthony Frank A. Ordoñez <143399325+AnthonyFrank-Ordonez@users.noreply.github.com>
2025-06-02 20:35:08 +0800

    change export prisma to export default

--- src/utils/prisma.ts
+++ src/utils/prisma.ts
@@ -1,6 +1,8 @@
 import { PrismaClient } from '@/generated/prisma';
 import { withAccelerate } from '@prisma/extension-accelerate';
 
-export const prisma = new PrismaClient({
+const prisma = new PrismaClient({
 	datasourceUrl: import.meta.env.DATABASE_URL,
 }).$extends(withAccelerate());
+
+export default prisma;
