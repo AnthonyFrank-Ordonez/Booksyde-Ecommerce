1565640ae25a77255e9e8c74a45f259e810541db
Anthony Frank A. Ordoñez <143399325+AnthonyFrank-Ordonez@users.noreply.github.com>
2025-06-03 20:17:24 +0800

    Added required prisma files

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,6 +1,11 @@
 import { betterAuth } from 'better-auth';
+import { prismaAdapter } from 'better-auth/adapters/prisma';
+import prisma from './prisma';
 
 export const auth = betterAuth({
+	database: prismaAdapter(prisma, {
+		provider: 'postgresql',
+	}),
 	socialProviders: {
 		github: {
 			clientId: process.env.GITHUB_CLIENT_ID! as string,
