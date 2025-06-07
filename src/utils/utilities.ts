9b16f754f5f0b3970b5d36ddde675fdfce5fd5c0
Anthony Frank A. Ordoñez <143399325+AnthonyFrank-Ordonez@users.noreply.github.com>
2025-06-07 16:35:27 +0800

    Setup nodemailer 2

--- src/utils/utilities.ts
+++ src/utils/utilities.ts
@@ -1,6 +1,10 @@
 import fs from 'fs';
 import path from 'path';
+import Handlebars from 'handlebars';
 
+// For Nodemailer
 export const emailTemplate = fs.readFileSync(
 	path.join(__dirname, '..', '..', 'template', 'verificationEmail.html')
 );
+
+export const compileTemplate = Handlebars.compile(emailTemplate);
