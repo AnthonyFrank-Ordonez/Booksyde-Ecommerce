4883489453e17f94b9854cf6e8ea31e4b28ebc9d
Anthony Frank A. Ordoñez <143399325+AnthonyFrank-Ordonez@users.noreply.github.com>
2025-06-07 09:49:59 +0800

    Update NotFound Component

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -1,11 +1,11 @@
 export function NotFound() {
 	return (
-		<div className='col-span-1 md:col-span-8'>
-			<div className='flex flex-col max-w-md md:max-w-2xl h-screent w-full h-screen mx-auto justify-center items-center gap-5'>
-				<h1 className='font-bold text-5xl md:text-7xl'>404 NOT FOUND</h1>
-				<p className='font-normal text-lg text-center max-w-lg'>
+		<div className='col-span-1 md:col-span-12'>
+			<div className='mx-auto flex h-screen w-full max-w-md flex-col items-center justify-center gap-5 md:max-w-2xl'>
+				<h1 className='text-5xl font-bold md:text-7xl'>404 NOT FOUND</h1>
+				<p className='max-w-lg text-center text-lg font-normal'>
 					The page you are looking for is not registered on our website. Please
-					check the URL or return to the homepage.
+					check the URL or return to our homepage.
 				</p>
 			</div>
 		</div>
