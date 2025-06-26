51df6e455bdccfa098366a906360aeef5e147e5e
Anthony Frank A. Ordoñez <143399325+AnthonyFrank-Ordonez@users.noreply.github.com>
2025-06-26 15:21:33 +0800

    Added ui and ux for medium screens

--- src/routes/products/books/$slug.tsx
+++ src/routes/products/books/$slug.tsx
@@ -22,22 +22,24 @@ function RouteComponent() {
 	const book = booksQueryData.data;
 
 	return (
-		<div className='grid grid-cols-1'>
-			<div className='grid grid-cols-1 px-6 py-5 sm:mx-auto sm:max-w-2xl'>
+		<div className='col-span-1 md:col-span-12'>
+			<div className='px-6 pt-5 sm:mx-auto sm:max-w-2xl md:max-w-4xl'>
 				<Link to='/products/books' className='mb-5 flex items-center'>
 					<MdArrowBackIos />
 					<h2 className='text-lg'>Go Back</h2>
 				</Link>
 
-				<div className='mb-3 flex items-center text-[11px] sm:text-[15px]'>
+				<div className='flex items-center text-[11px] sm:text-[15px]'>
 					<p>Products</p>
 					<MdArrowRight />
 					<p>Books</p>
 					<MdArrowRight />
 					<p>{book.title}</p>
 				</div>
+			</div>
 
-				<div className='relative mb-3 flex h-auto w-auto overflow-hidden rounded-sm bg-gray-200/90 px-5 py-5 sm:px-8 sm:py-8'>
+			<div className='grid grid-cols-1 px-6 py-3 sm:mx-auto sm:max-w-2xl md:max-w-4xl md:grid-cols-2 md:gap-10'>
+				<div className='relative mb-3 flex h-auto w-auto overflow-hidden rounded-sm bg-gray-200/90 px-5 py-5 sm:px-8 sm:py-8 md:max-h-[35rem] md:min-h-[35rem]'>
 					<img
 						src={book.coverImg}
 						alt={`${book.title} image`}
@@ -53,12 +55,12 @@ function RouteComponent() {
 					<p className='mb-1 text-sm text-gray-500 sm:mb-1.5'>{book.author}</p>
 
 					<div className='mb-2 flex items-center gap-1 sm:mb-3'>
-						<FaStar className='h-4 w-4 text-yellow-400 sm:h-5 sm:w-5' />
-						<FaStar className='h-4 w-4 text-yellow-400 sm:h-5 sm:w-5' />
-						<FaStar className='h-4 w-4 text-yellow-400 sm:h-5 sm:w-5' />
-						<FaStar className='h-4 w-4 text-yellow-400 sm:h-5 sm:w-5' />
-						<FaRegStar className='h-4 w-4 text-yellow-400 sm:h-5 sm:w-5' />
-						<p className='text-sm sm:text-lg'>({book.rating})</p>
+						<FaStar className='h-4 w-4 text-yellow-400 sm:h-5 sm:w-5 md:h-4 md:w-4' />
+						<FaStar className='h-4 w-4 text-yellow-400 sm:h-5 sm:w-5 md:h-4 md:w-4' />
+						<FaStar className='h-4 w-4 text-yellow-400 sm:h-5 sm:w-5 md:h-4 md:w-4' />
+						<FaStar className='h-4 w-4 text-yellow-400 sm:h-5 sm:w-5 md:h-4 md:w-4' />
+						<FaRegStar className='h-4 w-4 text-yellow-400 sm:h-5 sm:w-5 md:h-4 md:w-4' />
+						<p className='text-sm sm:text-lg md:text-sm'>({book.rating})</p>
 					</div>
 
 					<div className='mb-5 flex items-center gap-2'>
@@ -70,24 +72,26 @@ function RouteComponent() {
 					</div>
 
 					{/* Description */}
-					<p className='mb-0.5 font-light text-gray-500 sm:text-lg'>
-						Description
-					</p>
-					<div className='mb-1 border-b border-b-gray-300'></div>
-					<div className='mb-8'>
-						<p
-							className={`text-sm font-light text-gray-500 sm:text-[17px] ${!descExpanded ? 'line-clamp-4' : ''}`}
-						>
-							{book.description}
+					<div className='mb-8 md:mb-10'>
+						<p className='mb-0.5 font-light text-gray-500 sm:text-lg'>
+							Description
 						</p>
-						{book.description && book.description.length > 150 && (
-							<button
-								onClick={() => setDescExpanded(!descExpanded)}
-								className='mt-1 cursor-pointer text-xs font-medium text-black hover:text-black/70 focus:outline-none sm:text-sm'
+						<div className='mb-1 border-b border-b-gray-300'></div>
+						<div>
+							<p
+								className={`text-sm font-light text-gray-500 sm:text-[16px] md:text-[15px] ${!descExpanded ? 'line-clamp-4 md:line-clamp-7' : ''}`}
 							>
-								{descExpanded ? 'See Less' : 'See More'}
-							</button>
-						)}
+								{book.description}
+							</p>
+							{book.description && book.description.length > 150 && (
+								<button
+									onClick={() => setDescExpanded(!descExpanded)}
+									className='mt-1 cursor-pointer text-xs font-medium text-black hover:text-black/70 focus:outline-none sm:text-sm'
+								>
+									{descExpanded ? 'See Less' : 'See More'}
+								</button>
+							)}
+						</div>
 					</div>
 
 					{/* Add to Cart/Wishlist Buttons */}
