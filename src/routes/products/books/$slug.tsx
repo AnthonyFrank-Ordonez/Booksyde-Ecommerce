c40ebe7eb25b0f6600c8f1711aba751c054c6c3f
Anthony Frank A. Ordoñez <143399325+AnthonyFrank-Ordonez@users.noreply.github.com>
2025-06-26 15:50:38 +0800

    Finish slug route ui and ux

--- src/routes/products/books/$slug.tsx
+++ src/routes/products/books/$slug.tsx
@@ -23,22 +23,26 @@ function RouteComponent() {
 
 	return (
 		<div className='col-span-1 md:col-span-12'>
-			<div className='px-6 pt-5 sm:mx-auto sm:max-w-2xl md:max-w-4xl'>
+			<div className='px-6 pt-5 sm:mx-auto sm:max-w-2xl md:max-w-4xl lg:max-w-5xl xl:max-w-[78rem] 2xl:max-w-[90rem]'>
 				<Link to='/products/books' className='mb-5 flex items-center'>
 					<MdArrowBackIos />
 					<h2 className='text-lg'>Go Back</h2>
 				</Link>
 
 				<div className='flex items-center text-[11px] sm:text-[15px]'>
-					<p>Products</p>
+					<Link to='/products' className='hover:underline'>
+						Products
+					</Link>
 					<MdArrowRight />
-					<p>Books</p>
+					<Link to='/products/books' className='hover:underline'>
+						Books
+					</Link>
 					<MdArrowRight />
 					<p>{book.title}</p>
 				</div>
 			</div>
 
-			<div className='grid grid-cols-1 px-6 py-3 sm:mx-auto sm:max-w-2xl md:max-w-4xl md:grid-cols-2 md:gap-10'>
+			<div className='grid grid-cols-1 px-6 py-3 sm:mx-auto sm:max-w-2xl md:max-w-4xl md:grid-cols-2 md:gap-10 lg:max-w-5xl lg:grid-cols-[380px_3fr] xl:max-w-[78rem] 2xl:max-w-[90rem]'>
 				<div className='relative mb-3 flex h-auto w-auto overflow-hidden rounded-sm bg-gray-200/90 px-5 py-5 sm:px-8 sm:py-8 md:max-h-[35rem] md:min-h-[35rem]'>
 					<img
 						src={book.coverImg}
@@ -49,44 +53,50 @@ function RouteComponent() {
 
 				{/* Book Info */}
 				<div className='px-2'>
-					<h2 className='mb-1 text-2xl font-bold sm:mb-1.5 sm:text-3xl'>
+					<h2 className='mb-1 text-2xl font-bold sm:mb-1.5 sm:text-3xl xl:text-4xl 2xl:text-5xl'>
 						{book.title}
 					</h2>
-					<p className='mb-1 text-sm text-gray-500 sm:mb-1.5'>{book.author}</p>
+					<p className='mb-1 text-sm text-gray-500 sm:mb-1.5 xl:text-lg 2xl:text-xl'>
+						{book.author}
+					</p>
 
 					<div className='mb-2 flex items-center gap-1 sm:mb-3'>
-						<FaStar className='h-4 w-4 text-yellow-400 sm:h-5 sm:w-5 md:h-4 md:w-4' />
-						<FaStar className='h-4 w-4 text-yellow-400 sm:h-5 sm:w-5 md:h-4 md:w-4' />
-						<FaStar className='h-4 w-4 text-yellow-400 sm:h-5 sm:w-5 md:h-4 md:w-4' />
-						<FaStar className='h-4 w-4 text-yellow-400 sm:h-5 sm:w-5 md:h-4 md:w-4' />
-						<FaRegStar className='h-4 w-4 text-yellow-400 sm:h-5 sm:w-5 md:h-4 md:w-4' />
-						<p className='text-sm sm:text-lg md:text-sm'>({book.rating})</p>
+						<FaStar className='h-4 w-4 text-yellow-400 sm:h-5 sm:w-5 md:h-4 md:w-4 xl:h-5 xl:w-5' />
+						<FaStar className='h-4 w-4 text-yellow-400 sm:h-5 sm:w-5 md:h-4 md:w-4 xl:w-5' />
+						<FaStar className='h-4 w-4 text-yellow-400 sm:h-5 sm:w-5 md:h-4 md:w-4 xl:w-5' />
+						<FaStar className='h-4 w-4 text-yellow-400 sm:h-5 sm:w-5 md:h-4 md:w-4 xl:w-5' />
+						<FaRegStar className='h-4 w-4 text-yellow-400 sm:h-5 sm:w-5 md:h-4 md:w-4 xl:w-5' />
+						<p className='text-sm sm:text-lg md:text-sm xl:text-lg'>
+							({book.rating})
+						</p>
 					</div>
 
-					<div className='mb-5 flex items-center gap-2'>
-						<p className='text-2xl font-bold sm:text-3xl'>${book.price}</p>
-						<p className='text-2xl font-light text-gray-400 line-through sm:text-3xl'>
+					<div className='mb-5 flex items-center gap-2 xl:mb-8'>
+						<p className='text-2xl font-bold sm:text-3xl xl:text-4xl 2xl:text-5xl'>
+							${book.price}
+						</p>
+						<p className='text-2xl font-light text-gray-400 line-through sm:text-3xl xl:text-4xl 2xl:text-5xl'>
 							$10.38
 						</p>
-						<RiCoupon3Line className='mt-1 h-6 w-6' />
+						<RiCoupon3Line className='mt-1 h-6 w-6 xl:h-7 xl:w-7' />
 					</div>
 
 					{/* Description */}
 					<div className='mb-8 md:mb-10'>
-						<p className='mb-0.5 font-light text-gray-500 sm:text-lg'>
+						<p className='mb-0.5 font-light text-gray-500 sm:text-lg xl:mb-1 xl:text-xl'>
 							Description
 						</p>
 						<div className='mb-1 border-b border-b-gray-300'></div>
 						<div>
 							<p
-								className={`text-sm font-light text-gray-500 sm:text-[16px] md:text-[15px] ${!descExpanded ? 'line-clamp-4 md:line-clamp-7' : ''}`}
+								className={`text-sm font-light text-gray-500 sm:text-[16px] md:text-[15px] ${!descExpanded ? 'line-clamp-4 md:line-clamp-7 xl:line-clamp-0' : ''}`}
 							>
 								{book.description}
 							</p>
-							{book.description && book.description.length > 150 && (
+							{book.description && book.description.length > 350 && (
 								<button
 									onClick={() => setDescExpanded(!descExpanded)}
-									className='mt-1 cursor-pointer text-xs font-medium text-black hover:text-black/70 focus:outline-none sm:text-sm'
+									className='mt-1 cursor-pointer text-xs font-medium text-black hover:text-black/70 focus:outline-none sm:text-sm xl:hidden'
 								>
 									{descExpanded ? 'See Less' : 'See More'}
 								</button>
@@ -96,11 +106,11 @@ function RouteComponent() {
 
 					{/* Add to Cart/Wishlist Buttons */}
 					<div className='flex flex-col gap-3'>
-						<button className='w-full rounded-lg bg-black py-3 font-bold text-white'>
+						<button className='w-full cursor-pointer rounded-lg bg-black py-3 font-bold text-white transition-colors duration-300 hover:bg-black/80'>
 							Add to Cart
 						</button>
 
-						<button className='w-full rounded-lg border border-black bg-white py-3 font-bold text-black'>
+						<button className='w-full cursor-pointer rounded-lg border border-black bg-white py-3 font-bold text-black transition-colors duration-300 hover:bg-gray-400/20'>
 							Add to Wishlist
 						</button>
 					</div>
