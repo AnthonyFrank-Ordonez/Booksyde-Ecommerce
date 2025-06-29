8d208acb2097423f20ecd200ffbde30197681ec5
Anthony Frank A. Ordoñez <143399325+AnthonyFrank-Ordonez@users.noreply.github.com>
2025-06-29 21:08:44 +0800

    added radio button

--- src/routes/_settings/address.tsx
+++ src/routes/_settings/address.tsx
@@ -47,7 +47,7 @@ function Address() {
 	});
 
 	return (
-		<div className='px-4 py-3 md:py-5'>
+		<div className='px-4 py-3 md:px-7 md:py-5'>
 			<button
 				disabled={isFormOpen}
 				onClick={handleFormClick}
@@ -58,136 +58,175 @@ function Address() {
 			</button>
 
 			{isFormOpen && (
-				<>
+				<div>
 					<h2 className='mt-5 mb-5 text-2xl font-light'>Address Form</h2>
 
-					<div>
-						<form
-							onSubmit={(e) => {
-								e.preventDefault();
-								e.stopPropagation();
-								form.handleSubmit();
-							}}
-						>
-							{addressFormObj.map((obj) => (
-								<form.Field
-									key={obj.name}
-									name={obj.name}
-									children={(field) => (
-										<>
-											<label
-												aria-label={field.name}
-												htmlFor={field.name}
-												className='mb-1 block font-medium'
-											>
-												{obj.label} <FieldInfo field={field} />
-											</label>
-											<input
-												type='text'
-												aria-label={field.name}
-												name={field.name}
-												id={field.name}
-												value={field.state.value}
-												onChange={(e) => field.handleChange(e.target.value)}
-												required
-												className='mb-3 w-full rounded-md border border-gray-400 px-2 py-1 focus:border-transparent focus:ring-1 focus:outline-none'
-											/>
-										</>
-									)}
-								/>
-							))}
-
-							<form.Subscribe
-								selector={(state) => [state.canSubmit, state.isSubmitting]}
-								children={([canSubmit, isSubmitting]) => (
-									<div>
-										<button
-											type='submit'
-											aria-label='submit address btn'
-											disabled={!canSubmit || isSubmitting}
-											className='mt-3 mb-3 w-full cursor-pointer rounded-lg bg-black px-4 py-2 font-medium text-white transition-colors duration-300 hover:bg-black/85 focus:ring-1 focus:ring-gray-700 focus:ring-offset-2 focus:outline-none'
+					<form
+						onSubmit={(e) => {
+							e.preventDefault();
+							e.stopPropagation();
+							form.handleSubmit();
+						}}
+					>
+						{addressFormObj.map((obj) => (
+							<form.Field
+								key={obj.name}
+								name={obj.name}
+								children={(field) => (
+									<>
+										<label
+											aria-label={field.name}
+											htmlFor={field.name}
+											className='mb-1 block font-medium'
 										>
-											{isSubmitting ? (
-												<svg
-													xmlns='http://www.w3.org/2000/svg'
-													viewBox='0 0 200 200'
-													className='mx-auto h-7 w-7'
-												>
-													<rect
-														fill='#FFFFFF'
-														stroke='#FFFFFF'
-														strokeWidth='15'
-														width='30'
-														height='30'
-														x='25'
-														y='85'
-													>
-														<animate
-															attributeName='opacity'
-															calcMode='spline'
-															dur='2'
-															values='1;0;1;'
-															keySplines='.5 0 .5 1;.5 0 .5 1'
-															repeatCount='indefinite'
-															begin='-.4'
-														></animate>
-													</rect>
-													<rect
-														fill='#FFFFFF'
-														stroke='#FFFFFF'
-														strokeWidth='15'
-														width='30'
-														height='30'
-														x='85'
-														y='85'
-													>
-														<animate
-															attributeName='opacity'
-															calcMode='spline'
-															dur='2'
-															values='1;0;1;'
-															keySplines='.5 0 .5 1;.5 0 .5 1'
-															repeatCount='indefinite'
-															begin='-.2'
-														></animate>
-													</rect>
-													<rect
-														fill='#FFFFFF'
-														stroke='#FFFFFF'
-														strokeWidth='15'
-														width='30'
-														height='30'
-														x='145'
-														y='85'
-													>
-														<animate
-															attributeName='opacity'
-															calcMode='spline'
-															dur='2'
-															values='1;0;1;'
-															keySplines='.5 0 .5 1;.5 0 .5 1'
-															repeatCount='indefinite'
-															begin='0'
-														></animate>
-													</rect>
-												</svg>
-											) : (
-												'Add Address'
-											)}
-										</button>
-									</div>
+											{obj.label} <FieldInfo field={field} />
+										</label>
+										<input
+											type='text'
+											aria-label={`${field.name} input`}
+											name={field.name}
+											id={field.name}
+											value={field.state.value}
+											onChange={(e) => field.handleChange(e.target.value)}
+											required
+											className='mb-3 w-full rounded-md border border-gray-400 px-2 py-1 focus:border-transparent focus:ring-1 focus:outline-none'
+										/>
+									</>
 								)}
 							/>
-						</form>
+						))}
 
-						<button
-							onClick={handleCancelForm}
-							className='w-full cursor-pointer rounded-lg border border-gray-500 px-4 py-2 font-medium transition-colors duration-300 hover:bg-gray-500/5 focus:ring-1 focus:ring-gray-700 focus:ring-offset-2 focus:outline-none'
-						>
-							Cancel
-						</button>
-					</div>
-				</>
+						<form.Field
+							name='defaultAddress'
+							children={(field) => (
+								<div>
+									<label
+										aria-label={field.name}
+										htmlFor={field.name}
+										className='mt-1 mb-1 block font-medium'
+									>
+										Use as your default address?
+									</label>
+
+									<div className='mb-2 flex items-center gap-5'>
+										<label className='flex items-center gap-1 text-lg'>
+											<input
+												type='radio'
+												aria-label={`${field.name} input`}
+												name={field.name}
+												id={field.name}
+												checked={field.state.value === true}
+												onChange={() => field.handleChange(true)}
+											/>
+											Yes
+										</label>
+
+										<label className='flex items-center gap-1 text-lg'>
+											<input
+												type='radio'
+												aria-label={`${field.name} input`}
+												name={field.name}
+												id={field.name}
+												checked={field.state.value === false}
+												onChange={() => field.handleChange(false)}
+											/>
+											No
+										</label>
+									</div>
+								</div>
+							)}
+						/>
+
+						<form.Subscribe
+							selector={(state) => [state.canSubmit, state.isSubmitting]}
+							children={([canSubmit, isSubmitting]) => (
+								<div>
+									<button
+										type='submit'
+										aria-label='submit address btn'
+										disabled={!canSubmit || isSubmitting}
+										className='mt-3 mb-3 w-full cursor-pointer rounded-lg bg-black px-4 py-2 font-medium text-white transition-colors duration-300 hover:bg-black/85 focus:ring-1 focus:ring-gray-700 focus:ring-offset-2 focus:outline-none'
+									>
+										{isSubmitting ? (
+											<svg
+												xmlns='http://www.w3.org/2000/svg'
+												viewBox='0 0 200 200'
+												className='mx-auto h-7 w-7'
+											>
+												<rect
+													fill='#FFFFFF'
+													stroke='#FFFFFF'
+													strokeWidth='15'
+													width='30'
+													height='30'
+													x='25'
+													y='85'
+												>
+													<animate
+														attributeName='opacity'
+														calcMode='spline'
+														dur='2'
+														values='1;0;1;'
+														keySplines='.5 0 .5 1;.5 0 .5 1'
+														repeatCount='indefinite'
+														begin='-.4'
+													></animate>
+												</rect>
+												<rect
+													fill='#FFFFFF'
+													stroke='#FFFFFF'
+													strokeWidth='15'
+													width='30'
+													height='30'
+													x='85'
+													y='85'
+												>
+													<animate
+														attributeName='opacity'
+														calcMode='spline'
+														dur='2'
+														values='1;0;1;'
+														keySplines='.5 0 .5 1;.5 0 .5 1'
+														repeatCount='indefinite'
+														begin='-.2'
+													></animate>
+												</rect>
+												<rect
+													fill='#FFFFFF'
+													stroke='#FFFFFF'
+													strokeWidth='15'
+													width='30'
+													height='30'
+													x='145'
+													y='85'
+												>
+													<animate
+														attributeName='opacity'
+														calcMode='spline'
+														dur='2'
+														values='1;0;1;'
+														keySplines='.5 0 .5 1;.5 0 .5 1'
+														repeatCount='indefinite'
+														begin='0'
+													></animate>
+												</rect>
+											</svg>
+										) : (
+											'Add Address'
+										)}
+									</button>
+								</div>
+							)}
+						/>
+					</form>
+
+					<button
+						onClick={handleCancelForm}
+						className='w-full cursor-pointer rounded-lg border border-gray-500 px-4 py-2 font-medium transition-colors duration-300 hover:bg-gray-500/5 focus:ring-1 focus:ring-gray-700 focus:ring-offset-2 focus:outline-none'
+					>
+						Cancel
+					</button>
+				</div>
 			)}
 		</div>
 	);
