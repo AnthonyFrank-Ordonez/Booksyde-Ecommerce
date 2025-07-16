f331dd438c121bb794964f9444851de3fb0d72b6
Anthony Frank A. Ordoñez <143399325+AnthonyFrank-Ordonez@users.noreply.github.com>
2025-07-16 16:15:48 +0800

    Implemented prettier and temporarily remove the tsc on github action

--- src/routes/products/novels/index.tsx
+++ src/routes/products/novels/index.tsx
@@ -1,9 +1,9 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/products/novels/')({
-  component: RouteComponent,
-})
+	component: RouteComponent,
+});
 
 function RouteComponent() {
-  return <div>Hello "/products/novels/"!</div>
+	return <div>Hello "/products/novels/"!</div>;
 }
