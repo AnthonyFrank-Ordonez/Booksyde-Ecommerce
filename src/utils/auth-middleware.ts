39c01be5f127c95b747309acbfc4914cadc17515
Anthony Frank A. Ordoñez <143399325+AnthonyFrank-Ordonez@users.noreply.github.com>
2025-07-19 21:48:15 +0800

    Update auth-middleware.ts

--- src/utils/auth-middleware.ts
+++ src/utils/auth-middleware.ts
@@ -5,7 +5,6 @@ import { auth } from './auth';
 export const authMiddleware = createMiddleware({ type: 'function' }).server(
 	async ({ next }) => {
 		const request = getWebRequest();
-		console.log('🚀 ~ request:', request);
 
 		const session = await auth.api.getSession({
 			query: {
