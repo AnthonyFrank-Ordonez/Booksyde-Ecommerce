799e55e8c91a5465821fe22a6b3e817f4a8d6415
Anthony Frank A. Ordoñez <143399325+AnthonyFrank-Ordonez@users.noreply.github.com>
2025-07-24 15:06:49 +0800

    Added server function for getting user wishlist

---
+++ src/utils/servers/wishlist.ts
@@ -0,0 +1,61 @@
+import { createServerFn } from '@tanstack/react-start';
+import { queryOptions } from '@tanstack/react-query';
+import { GetUserIdSchema } from '../zod';
+import prisma from '../prisma';
+
+const getOrCreateWishlistFn = createServerFn({ method: 'POST' })
+	.validator((data) => GetUserIdSchema.parse(data))
+	.handler(async ({ data }) => {
+		const currentUserId = data.userId;
+
+		const wishlist = await prisma.$transaction(async (tx) => {
+			// try to find the existing wishlist
+			let userWishlist = await tx.wishlist.findUnique({
+				where: { userId: currentUserId },
+				include: {
+					wishlists: {
+						include: {
+							book: true,
+						},
+					},
+				},
+			});
+
+			if (!userWishlist) {
+				userWishlist = await tx.wishlist.create({
+					data: { userId: currentUserId },
+					include: {
+						wishlists: {
+							include: {
+								book: true,
+							},
+						},
+					},
+				});
+			}
+
+			return userWishlist;
+		});
+
+		return {
+			...wishlist,
+			wishlists: wishlist.wishlists.map((item) => ({
+				...item,
+				book: item.book && {
+					...item.book,
+					price: item.book.price.toJSON(),
+					rating: item.book.price.toJSON(),
+				},
+			})),
+		};
+	});
+
+export const useGetOrCreateWishlist = (userId: string) =>
+	queryOptions({
+		queryKey: ['wishlist', userId],
+		enabled: !!userId,
+		queryFn: getOrCreateWishlistFn,
+		staleTime: Infinity,
+		retry: 1,
+		refetchOnWindowFocus: false,
+	});
