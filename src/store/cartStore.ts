67cf74c01d6d4325c8e3c192d3d3249a918991f9
Anthony Frank A. Ordoñez <143399325+AnthonyFrank-Ordonez@users.noreply.github.com>
2025-08-01 16:14:13 +0800

    Update cartStore.ts

--- src/store/cartStore.ts
+++ src/store/cartStore.ts
@@ -3,12 +3,26 @@ import { persist } from 'zustand/middleware';
 
 interface CartStore {
 	checkedItemIds: Set<string>;
+
+	// Setters
+	clearItemIds: () => void;
+
+	// Getters
+	isItemChecked: (id: string) => boolean;
 }
 
 export const useCartStore = create<CartStore>()(
 	persist(
 		(set, get) => ({
 			checkedItemIds: new Set(),
+
+			// Setters
+			clearItemIds: () => set({ checkedItemIds: new Set() }),
+
+			// Getters
+			isItemChecked: (id: string) => {
+				return get().checkedItemIds.has(id);
+			},
 		}),
 		{
 			name: 'cart-store',
